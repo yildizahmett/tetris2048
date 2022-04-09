508dbb71e25941faf616063f372307ca436d7b05
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2022-04-09 22:51:59 +0300

    Update tetris 2048

--- Tetris_2048.py
+++ Tetris_2048.py
@@ -10,22 +10,39 @@ import random # used for creating tetrominoes with random types/shapes
 #-------------------------------------------------------------------------------
 # Main function where this program starts execution
 def start():
-   # set the dimensions of the game grid
-   grid_h, grid_w = 20, 12
-   # set the size of the drawing canvas
-   canvas_h, canvas_w = 40 * grid_h, 60 * grid_w
-   stddraw.setCanvasSize(canvas_w, canvas_h) 
+   # fake set the size of the window for centering game grid canvas
+   stddraw.setCanvasSize(800, 800)
+   # set main window canvas
+   stddraw.setCanvasSize(500, 500)
    # set the scale of the coordinate system
-   stddraw.setXscale(-0.5, grid_w + 10)
+   stddraw.setXscale(-0.5, 16.5)
+   stddraw.setYscale(-0.5, 15.5)
+   # display a simple menu before opening the game
+   # by using the display_game_menu function defined below
+   display_game_menu(16, 17)
+   # set game settings
+   game_speed, grid_results, game_type = prepare_screen()
+
+   # set the dimensions of the game grid
+   grid_h, grid_w = grid_results[1], grid_results[0]
+   info_w = 5 # do not change this value
+   game_w = grid_w + info_w
+   box_radius = 40 # do not change this value
+   # set the size of the drawing canvas
+   canvas_h, canvas_w = box_radius * grid_h, box_radius * game_w
+   # set the game grid canvas
+   stddraw.setCanvasSize(canvas_w, canvas_h)
+   # set the scale of the coordinate system
+   stddraw.setXscale(-0.5, game_w - 0.5)
    stddraw.setYscale(-0.5, grid_h - 0.5)
 
    # set the dimension values stored and used in the Tetromino class
    Tetromino.grid_height = grid_h
    Tetromino.grid_width = grid_w
-   
+
    # create the game grid
    grid = GameGrid(grid_h, grid_w)
-   # create the first tetromino to enter the game grid 
+   # create the first tetromino to enter the game grid
    # by using the create_tetromino function defined below
    current_tetromino = create_tetromino(grid_h, grid_w)
    grid.current_tetromino = current_tetromino
@@ -33,8 +50,8 @@ def start():
    # display a simple menu before opening the game
    # by using the display_game_menu function defined below
    display_game_menu(grid_h, grid_w + 10)
-   
-   # the main game loop (keyboard interaction for moving the tetromino) 
+
+   # the main game loop (keyboard interaction for moving the tetromino)
    while True:
       # check user interactions via the keyboard
       if stddraw.hasNextKeyTyped():  # check if the user has pressed a key
@@ -43,14 +60,14 @@ def start():
          # if the left arrow key has been pressed
          if key_typed == "left":
             # move the active tetromino left by one
-            current_tetromino.move(key_typed, grid) 
+            current_tetromino.move(key_typed, grid)
          # if the right arrow key has been pressed
          elif key_typed == "right":
             # move the active tetromino right by one
             current_tetromino.move(key_typed, grid)
          # if the down arrow key has been pressed
          elif key_typed == "down":
-            # move the active tetromino down by one 
+            # move the active tetromino down by one
             # (soft drop: causes the tetromino to fall down faster)
             current_tetromino.move(key_typed, grid)
          # clear the queue of the pressed keys for a smoother interaction
@@ -69,13 +86,13 @@ def start():
          # end the main game loop if the game is over
          if game_over:
             break
-         # Check if the 
+         # Check if the
          # create the next tetromino to enter the game grid
          # by using the create_tetromino function defined below
          current_tetromino = create_tetromino(grid_h, grid_w)
          grid.current_tetromino = current_tetromino
 
-      # display the game grid and the current tetromino      
+      # display the game grid and the current tetromino
       grid.display()
 
    # print a message on the console when the game is over
@@ -111,7 +128,7 @@ def display_game_menu(grid_height, grid_width):
    stddraw.picture(image_to_display, img_center_x, img_center_y)
    # dimensions of the start game button
    button_w, button_h = grid_width - 1.5, 2
-   # coordinates of the bottom left corner of the start game button 
+   # coordinates of the bottom left corner of the start game button
    button_blc_x, button_blc_y = img_center_x - button_w / 2, 4
    # display the start game button as a filled rectangle
    stddraw.setPenColor(button_color)
@@ -128,15 +145,108 @@ def display_game_menu(grid_height, grid_width):
       stddraw.show(50)
       # check if the mouse has been left-clicked on the button
       if stddraw.mousePressed():
-         # get the x and y coordinates of the location at which the mouse has 
-         # most recently been left-clicked  
+         # get the x and y coordinates of the location at which the mouse has
+         # most recently been left-clicked
          mouse_x, mouse_y = stddraw.mouseX(), stddraw.mouseY()
          # check if these coordinates are inside the button
          if mouse_x >= button_blc_x and mouse_x <= button_blc_x + button_w:
-            if mouse_y >= button_blc_y and mouse_y <= button_blc_y + button_h: 
+            if mouse_y >= button_blc_y and mouse_y <= button_blc_y + button_h:
                break # break the loop to end the method and start the game
 
-# start() function is specified as the entry point (main function) from which 
+# Function for displaying the preparing menu
+def prepare_screen():
+
+   background_color = Color(42, 69, 99); button_color = Color(25, 255, 228); text_color = Color(31, 160, 239)
+
+   pixelToCoordinate = lambda x, in_min, in_max, out_min, out_max : int( round((x-in_min)*(out_max-out_min)/(in_max-in_min)+out_min) )
+
+   scale = 500
+   stddraw.setXscale(0,scale)
+   stddraw.setYscale(0,scale)
+
+   gridDimensions = [280,280]
+   gridResults = [14, 20]
+
+   gameSpeed = 160
+   game_type = ""
+   while True:
+      stddraw.clear(background_color)
+      stddraw.setPenColor(Color(0,0,0))
+      stddraw.filledRectangle(280-300/2,450,300,10)
+      stddraw.filledRectangle(280-300/2,400,300,10)
+
+      stddraw.setPenColor(button_color)
+      stddraw.rectangle(280-300/2,450,300.5,10.5)
+      stddraw.rectangle(280-300/2,400,300.5,10.5)
+
+      stddraw.setPenColor(Color(255,255,255))
+      stddraw.filledRectangle(120-75/2,315,85,50)
+      stddraw.filledRectangle(245-75/2,315,85,50)
+      stddraw.filledRectangle(370-75/2,315,85,50)
+
+      stddraw.setPenColor(button_color)
+      stddraw.filledRectangle(125-75/2,320,75,40)
+      stddraw.filledRectangle(250-75/2,320,75,40)
+      stddraw.filledRectangle(375-75/2,320,75,40)
+
+      stddraw.setPenColor(Color(255,255,255))
+      stddraw.filledRectangle(245-350/2,55,360,230)
+
+      stddraw.setPenColor(button_color)
+      stddraw.filledRectangle(250-350/2,60,350,220)
+
+      stddraw.setFontFamily("Arial")
+      stddraw.setFontSize(16)
+      stddraw.setPenColor(Color(0,0,0))
+
+      stddraw.boldText(125,340,"EASY")
+      stddraw.boldText(250,340,"MEDIUM")
+      stddraw.boldText(375,340,"HARD")
+
+      stddraw.setPenColor(background_color)
+      stddraw.filledCircle(gridDimensions[0],455,18)
+      stddraw.filledCircle(gridDimensions[1],405,18)
+      stddraw.setPenColor(Color(255,255,255))
+      stddraw.filledCircle(gridDimensions[0],455,15)
+      stddraw.filledCircle(gridDimensions[1],405,15)
+      stddraw.setFontSize(14)
+      stddraw.boldText(100,457,"Width :")
+      stddraw.boldText(100,407,"Height :")
+      stddraw.setFontSize(16)
+      stddraw.setPenColor(Color(0,0,0))
+      stddraw.text(gridDimensions[0],455,str(int(gridResults[0])))
+      stddraw.text(gridDimensions[1],405,str(int(gridResults[1])))
+
+      stddraw.show(50)
+
+      if stddraw.mousePressed():
+
+         mouse_x, mouse_y = stddraw.mouseX(), stddraw.mouseY()
+
+         if mouse_x >= 280-300/2 and mouse_x <= 280-300/2 + 300:
+               if (mouse_y >= (450) - 15 and mouse_y <= (450) + 20):
+                  gridDimensions[0] = (mouse_x)
+                  gridResults[0] = pixelToCoordinate(mouse_x,130,430,8,20)
+               if (mouse_y >= (400) - 15 and mouse_y <= (400) + 20):
+                  gridDimensions[1] = (mouse_x)
+                  gridResults[1] = pixelToCoordinate(mouse_x,130,430,16,24)
+
+
+         if (mouse_y >= (320) and mouse_y <= (320) + 40) :
+               if (mouse_x >= 125-75/2 and mouse_x <= 125-75/2 + 40):
+                  gameSpeed = 400
+                  game_type = "easy"
+               if (mouse_x >= 250-75/2 and mouse_x <= 250-75/2 + 40):
+                  gameSpeed = 250
+                  game_type = "medium"
+               if (mouse_x >= 375-75/2 and mouse_x <= 375-75/2 + 40):
+                  gameSpeed = 150
+                  game_type = "hard"
+
+               if (gameSpeed != 0) :
+                  return gameSpeed, tuple(gridResults), game_type
+
+# start() function is specified as the entry point (main function) from which
 # the program starts execution
 if __name__== '__main__':
    start()
